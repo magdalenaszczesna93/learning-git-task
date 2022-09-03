9c7bc0eacadc81bf0cc42f714dbf4a9652ba6952
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-09-03 18:33:14 +0200

    add loop how item in store.py

--- Init project.py
+++ Init project.py
@@ -2,3 +2,6 @@ print("Lista zakupów")
 #Zadanie 1
 lista_dict = {"piekarnia":["chleb", "bułki", "pączek"], "warzywniak":["marchew", "seler", "rukola"]}
 rzeczy = lista_dict.values()
+for sklep in lista_dict:
+    print(f"Idę do {sklep} i kupuję: {lista_dict[sklep]}")
+#nie mogę sobie poradzić z tym, jak sformatować listę rzeczy
