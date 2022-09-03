9a983b1574d118ec85291989623be54969497cc9
Magdalena Szczesna <magdalena.szczesna.ue@gmil.com>
2022-09-03 18:25:09 +0200

    add dictionary

--- Init project.py
+++ Init project.py
@@ -2,13 +2,3 @@ print("Lista zakupów")
 #Zadanie 1
 lista_dict = {"piekarnia":["chleb", "bułki", "pączek"], "warzywniak":["marchew", "seler", "rukola"]}
 rzeczy = lista_dict.values()
-for sklep in lista_dict:
-    print(f"Idę do {sklep.capitalize()} i kupuję: {lista_dict[sklep]}")
-#nie mogę sobie poradzić z tym, jak sformatować listę rzeczy
-
-for rzeczy in lista_dict:
-    number = (len(lista_dict[rzeczy]))
-    print(type(number))
-#nie wiem czemu liczy tylko dla jednego skelpu
-    print(f"W sumie kupuję {number} produktów")
-print("\n")
